57dbc138ce2a6fffaa9eed5bfeb6460c65b5a46e
Jancarlos Sosa <60488065+Sosa-IQ@users.noreply.github.com>
2026-01-16 14:50:44 -0500

    add components

---
+++ components/UserDropdown.tsx
@@ -0,0 +1,45 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+
+
+const UserDropdown = () => {
+    const router = useRouter();
+
+    const handleSignOut = async () => {
+        router.push('/sign-in');
+    }
+
+    const user = { name: "John", email: "contact@email.com" };
+
+  return (
+    <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500">
+                <Avatar>
+                    {/* <AvatarImage src="/assets/images/user-avatar.jpg" alt={user.name} /> */}
+                    <AvatarFallback>{user.name[0]}</AvatarFallback>
+                </Avatar>
+            </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UserDropdown
